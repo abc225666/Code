463bef9ec171b4a236029590054026aba4c443bc
Wang Terry <qwe50120@gmail.com>
2016-10-18 02:33:00 +0800

    uva

---
+++ Uva/default.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+#include <math.h>
+
+//#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int lli;
+
+int main()
+{
+	return 0;
+}
